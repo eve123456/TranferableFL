Fix the bug for data normalization, the mean and std for mnist-m-train is [0.4549, 0.2208]

data/finetune/ft_loader.py
@@ -47,8 +47,9 @@ def get_loader(image_dir, dataset_name, batch_size, **kwargs):
     img_transform = transforms.Compose([
         transforms.Resize(28),
         transforms.ToTensor(),
-        lambda x: x * 255,
-        transforms.Grayscale()
+        # lambda x: x * 255,
+        transforms.Grayscale(),
+        transforms.Normalize((0.4549,), (0.2208,))  # These are the mean and std computed on mnist_m_train
     ])
 
     trainset = FTDataset(
@@ -69,7 +70,7 @@ def get_loader(image_dir, dataset_name, batch_size, **kwargs):
         shuffle=True,
         **kwargs)
 
-    test_loader = torch.utils.data.DataLoader(
+    test_loader = DataLoader(
         dataset=testset,
         batch_size=batch_size,
         shuffle=False,

src/utils/worker_utils.py
@@ -103,8 +103,7 @@ class MiniDataset(Dataset):
                  ]
             )
         elif self.data.ndim == 3:
-            # self.data = np.expand_dims(self.data, axis=1).astype(np.float64)
-            self.data = self.data.reshape((-1, 28, 28, 1)).astype("float32")
+            self.data = self.data.reshape((-1, 28, 28, 1)).astype("uint8")
             self.transform = transforms.Compose(
                 [transforms.ToTensor(),
                  transforms.Normalize((0.1307,), (0.3081,))